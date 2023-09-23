92cc2e3248299ff0a2c2b515ea89d59d73ae48e0
Soham Ali <soham@Sohams-MacBook-Air.local>
2023-09-23 16:55:38 -0400

    getHRs now returns statistics and plot

---
+++ R/Utils.R
@@ -0,0 +1,22 @@
+#' Extract Variables from a Formula
+#'
+#' @param formula Formula as a string.
+#'
+#' @return Variables extracted from the formula in a vector
+#'
+getFormulaVars <- function(formula) {
+  operation_chars <- c('\\+' = ',',
+                       '\\-' = ',',
+                       '\\*' = ',',
+                       '\\/' = ',',
+                       '\\^' = ',',
+                       '\\(' = ',',
+                       '\\)' = ',',
+                       '\\|' = ',')
+
+  vars <- unlist(strsplit(stringr::str_replace_all(formula,operation_chars),','))
+  vars <- vars[!(vars=='')]
+  vars <- vars[is.na(suppressWarnings(as.numeric(vars)))]
+
+  return(vars)
+}
