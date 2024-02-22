a15a61d15ecf3cf7030fd6a382151b1c8fcfc260
Soham Ali <soham@Sohams-MacBook-Air.local>
2024-02-21 19:58:47 -0500

    getHRs function shows rows for baseline as well

--- R/getHRs.R
+++ R/getHRs.R
@@ -19,14 +19,14 @@ getHRs <- function(data,
   event.time <- event.time[event.time %in% colnames(data)]
   event.status <- event.status[event.status %in% colnames(data)]
 
-  if(any(!length(event.time), !length(event.status))) stop(paste0('"',event.time,
+  if(any(!length(event.time), !length(event.status))) stop(paste0('"', event.time,
                                                                   '" and "', event.status,
                                                                   '" must be column names in data'))
 
   vars <- getFormulaVars(predictor_formula)
   na_terms <- vars[!(vars %in% colnames(data))]
-  if(length(na_terms)) stop(paste('The following terms are not variables (column names) in the data:',
-                                  paste(na_terms, sep = ',')))
+  if(length(na_terms)) stop(paste0('The following terms are not variables (column names) in the data:\n ',
+                                   paste(na_terms, sep = ',')))
 
   data[[event.time]] <- as.numeric(data[[event.time]])
   data[[event.status]] <- as.numeric(data[[event.status]])
@@ -38,13 +38,26 @@ getHRs <- function(data,
   fit <- survival::coxph(f, data = temp)
 
   if(length(vars) > 1) {
-    vars.values <- unlist(sapply(vars, function(v) {
+    # vars.groups <- vars.groups <- lapply(vars, function(v) {
+    #   list(baseline=levels(unique(as.factor(temp[[v]])))[1],
+    #        comparisons=levels(unique(as.factor(temp[[v]])))[2:length(unique(temp[[v]]))])
+    # })
+    # names(vars.groups) <- vars
+    #
+    # names(fit$coefficients) <- as.vector(unlist(lapply(vars.groups, function(v) v$comparisons)))
+
+    vars.comparisons <- sapply(vars, function(v) {
       levels(unique(as.factor(temp[[v]])))[2:length(unique(temp[[v]]))]
-    }))
+    })
+    vars.baselines <- lapply(vars, function(v) {
+      list(name = levels(unique(as.factor(temp[[v]])))[1],
+           n_comparisons = length(vars.comparisons[[v]]))
+    })
+    names(vars.baselines) <- vars
 
-    names(fit$coefficients) <- as.vector(vars.values)
+    names(fit$coefficients) <- as.vector(unlist(vars.comparisons))
 
-    res <- data.frame(t(sapply(as.vector(vars.values), function(v) {
+    res <- data.frame(t(sapply(as.vector(unlist(vars.comparisons)), function(v) {
       data.frame('HR'=unname(exp(coef(fit)[[v]])),
                  'CI.lower'=exp(confint(fit)[v,1]),
                  'CI.upper'=exp(confint(fit)[v,2]),
@@ -54,6 +67,33 @@ getHRs <- function(data,
                        'CI lower',
                        'CI upper',
                        'p-value')
+
+    temp <- res
+    r <- 1
+    for(v in vars.baselines) {
+      # Some rownames will get messed up, so we store them
+      rnames <- rownames(temp)
+
+      temp <- rbind(temp[0:(r-1),],
+                    c('-','-','-','-'),
+                    temp[(r):nrow(temp),])
+      rownames(temp)[r] <- v$name
+
+      # Fix the other rownames in case they got messed up
+      rownames(temp)[0:(r-1)] <- rnames[0:(r-1)]
+      rownames(temp)[(r+1):nrow(temp)] <- rnames[(r):length(rnames)]
+
+      r <- r + v$n_comparisons + 1
+    }
+
+    res <- lapply(vars.baselines, function(v) {
+      temp <- rbind(temp[r,],
+                    c('-','-','-','-'),
+                    temp[(r+1):nrow(temp),])
+      rownames(temp)[0] <- v$name
+      r <- r + v$n_comparisons + 1
+      return(temp)
+    })
   } else {
     res <- data.frame(matrix(ncol = 0,nrow = 1))
 
